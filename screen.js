1a0564b978d5e687232f36564858d527696f1ad5
Yurii Pavlenko <yurii.pavlenko@gmx.de>
2022-03-17 22:13:00 +0100

    added penis

--- screen.js
+++ screen.js
@@ -1,21 +1,21 @@
 const useScreen = () => {
-    // React is fun and games untill you're hooked (he-he)  :D
-    const output = document.getElementById("out");
-    const executeButton = document.getElementById("butt");
-    const flushButton = document.getElementById("flush");
-    
-    const flush = () => output.value = "";
-    flushButton.addEventListener("click", flush);
-    
-    let currentFunction = () => {};
-    const getOutput = () => currentFunction(document.getElementById("in").value);
-    executeButton.addEventListener("click", () => output.value =getOutput());
+  // React is fun and games untill you're hooked (he-he)  8======D O;
+  const output = document.getElementById("out");
+  const executeButton = document.getElementById("butt");
+  const flushButton = document.getElementById("flush");
 
-    return {
-        log: (func = (val) => val) =>{
-            currentFunction = func;
-        }
-    }
-}
+  const flush = () => (output.value = "");
+  flushButton.addEventListener("click", flush);
+
+  let currentFunction = () => {};
+  const getOutput = () => currentFunction(document.getElementById("in").value);
+  executeButton.addEventListener("click", () => (output.value = getOutput()));
+
+  return {
+    log: (func = (val) => val) => {
+      currentFunction = func;
+    },
+  };
+};
 
 export default useScreen;
